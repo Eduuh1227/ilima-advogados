c725834879fe34540e8a13ef55ff6b333d36ad87
Eduardo Oliveira <eduardocavallin2005@gmail.com>
2026-03-29 17:44:56 -0300

    feat: adding images

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const mobileMenu = document.getElementById('mobileMenu');
 const mobileClose = document.getElementById('mobileClose');
 const mobileBackdrop = document.getElementById('mobileBackdrop');
 const mobileLinks = mobileMenu.querySelectorAll('a');
+const imgPrata = document.getElementById("imgPrata");
+const imgBege = document.getElementById("imgBege");
 
+
+
+revealElements.forEach((el) => observer.observe(el));
 function openMenu() {
     mobileMenu.classList.add('open');
     mobileBackdrop.classList.add('show');
@@ -14,6 +19,22 @@ function closeMenu() {
     mobileMenu.classList.remove('open');
     mobileBackdrop.classList.remove('show');
     document.body.style.overflow = '';
+}
+
+function trocarImagem(el) {
+  const mainImage = document.getElementById('mainImage');
+  const thumbs = document.querySelectorAll('.empresarial-thumbs img');
+
+ mainImage.style.opacity = 0;
+
+  setTimeout(() => {
+    mainImage.src = el.src;
+    mainImage.alt = el.alt;
+    mainImage.style.opacity = 1;
+  }, 200);
+
+  thumbs.forEach(img => img.classList.remove('active'));
+  el.classList.add('active');
 }
 
 menuToggle?.addEventListener('click', openMenu);
